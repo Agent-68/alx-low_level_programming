860c0ea952edd0268dc228fdb597eb76dad4d31e
Agent-68 <gusengao@protonmail.com>
2023-02-16 06:14:02 -0800

    correction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,6 @@
-include <unistd.h>
+nclude <unistd.h>
 int main (void)
 {
-		write (1,"and that piece of art is useful\" - Dora Korpar, 2015-10-19",59);
-			return(1);
+        write (1,"and that piece of art is useful\" - Dora Korpar, 2015-10-19",59);
+        return(1);
 }
