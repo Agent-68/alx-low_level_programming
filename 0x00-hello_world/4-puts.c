93f9dde6e8481dba6169052127cef1d6bf8257d1
Agent-68 <gusengao@protonmail.com>
2023-02-16 06:48:43 -0800

    betty again

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+
+/*
+ *input main
+ * return 0 always
+ */
+
+
 int main (void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
